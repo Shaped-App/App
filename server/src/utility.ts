92bd7d54a7a41151a9e3e5ab44450576dc8b1063
xumaple <45406854+xumaple@users.noreply.github.com>
2020-07-09 13:32:06 -0400

    Added utility functions for generating random strings (#9)

---
+++ server/src/utility.ts
@@ -0,0 +1,17 @@
+
+
+export function generateRandomString(length: number) {
+  let s = "";
+  while (s.length < length) {
+    s += Math.random().toString(36).replace(/[^a-z]+/g, '');
+  }
+  return s.substr(0, length);
+}
+
+export function min(a: number, b: number) {
+  return a < b ? a : b;
+}
+
+export function max(a: number, b: number) {
+  return a > b ? a : b;
+}
