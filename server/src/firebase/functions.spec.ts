aaad4afb91a74be4d46312af4897d5546757fd39
Maple Xu <maplexu2010@gmail.com>
2020-07-10 23:59:15 -0400

    Added basic firebase support and utility functions

---
+++ server/src/firebase/functions.spec.ts
@@ -0,0 +1,31 @@
+import {
+    getDocuments,
+    getDocumentData,
+    getCollectionData
+} from './functions';
+import db from './model';
+
+describe('Firebase functions tests', () => {
+  describe('Testing getDocuments', () => {
+    it('Should find doc_id1, doc_id2', async () => {
+      expect(await getDocuments('test')).toMatchObject(['doc_id1', 'doc_id2']);
+    });
+  });
+
+  describe('Testing getDocumentData', () => {
+    it('Should find { doc_field1: doc_value1, doc_field2: doc_value2 }', async () => {
+      expect(await getDocumentData('test', 'doc_id1')).toMatchObject({ 'doc_field1': 'doc_value1', 'doc_field2': 'doc_value2' });
+    });
+
+    it('Should find { doc_field3: doc_value3 }', async () => {
+      expect(await getDocumentData('test', 'doc_id2')).toMatchObject({ 'doc_field3': 'doc_value3' });
+    });
+  });
+
+  describe('Testing getCollectionData', () => {
+    it('Should find { doc_field1: doc_value1, doc_field2: doc_value2, doc_field3: doc_value3 }', async() => {
+      expect(await getCollectionData('test')).toMatchObject({ 'doc_field1': 'doc_value1', 'doc_field2': 'doc_value2', 'doc_field3': 'doc_value3' })
+    })
+  })
+});
+  
