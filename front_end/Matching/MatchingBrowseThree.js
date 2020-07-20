9283b3be398b53d4cb8e69b90145a270af91e3ea
Hans Sy <55111820+hanssy130@users.noreply.github.com>
2020-07-19 21:38:56 -0700

    Cleaned up style according to meeting notes.

--- front_end/Matching/MatchingBrowseThree.js
+++ front_end/Matching/MatchingBrowseThree.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {SafeAreaView, View, Text, TouchableOpacity} from 'react-native';
 
-import styles from './Static/style.js';
+import styles from '../Static/style.js';
 
 export default class BrowseThreeScreen extends Component {
   constructor(props) {
@@ -12,38 +12,13 @@ export default class BrowseThreeScreen extends Component {
     return (
       <SafeAreaView style={styles.match__background}>
         <View style={styles.screen__header} />
-        <View
-          style={[
-            styles.content__container,
-            {
-              // TODO - Add these to the style. For review.
-              height: '90%',
-              justifyContent: 'center',
-              alignItems: 'center',
-            },
-          ]}>
+        <View style={[styles.content__container, styles.content__centering]}>
           <Text style={[{fontWeight: 'bold', fontSize: 20, margin: 10}]}>
             Success!
           </Text>
           <Text>Your response has been recorded.</Text>
           <Text>Now explore what others have said.</Text>
-          <TouchableOpacity
-            style={[
-              styles.button,
-              {
-                margin: 30,
-                // Created from https://ethercreative.github.io/react-native-shadow-generator/
-                shadowColor: '#000',
-                shadowOffset: {
-                  width: 0,
-                  height: 3,
-                },
-                shadowOpacity: 0.29,
-                shadowRadius: 4.65,
-
-                elevation: 7,
-              },
-            ]}>
+          <TouchableOpacity style={[styles.button, {margin: 30}]}>
             <Text style={styles.text__header}>Let's go</Text>
           </TouchableOpacity>
         </View>
