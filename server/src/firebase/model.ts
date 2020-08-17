76bb887a4b78a4557a62d7a8a56bf762fcddba53
xumaple <45406854+xumaple@users.noreply.github.com>
2020-08-17 12:49:15 -0400

    Revert "Answer and question endpoints (#30)" (#31)
    This reverts commit 230eb5c3180a7caa65fbbd95c0d075242488d01d.

--- server/src/firebase/model.ts
+++ server/src/firebase/model.ts
@@ -1,20 +1,15 @@
-import admin from 'firebase-admin';
-import { ROOT_DIR } from '../const';
+import { ROOT_DIR } from '../const'
 
 console.log('***** Loading firestore database *****');
+var admin = require('firebase-admin');
 
-const serviceAccount = require(ROOT_DIR + 'firebase_key.json');
+var serviceAccount = require(ROOT_DIR + 'firebase_key.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: 'https://shaped.firebaseio.com'
 });
 
-const db = admin.firestore();
+var db = admin.firestore();
 console.log('Loaded.');
 export default db;
-
-export const QuestionCollection: admin.firestore.CollectionReference = db.collection("questions");
-export const col: admin.firestore.CollectionReference = db.collection("questions");
-export const UserCollection: admin.firestore.CollectionReference = db.collection("users");
-
